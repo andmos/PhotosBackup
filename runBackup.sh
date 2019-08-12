1224f582bb225da8c388521749cdb0418ea73c40
andmos <andreas.mosti@gmail.com>
2019-08-12 21:06:57 +0200

    make sure we have fullpath and path variable exposed

--- runBackup.sh
+++ runBackup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-configFile=config.json
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+configFile=$dir/config.json
 backupFolder=$(cat $configFile |jq .backupFolder -r)
 
 for backupDest in $(cat $configFile |jq '.backupDestionations[]' -r)
